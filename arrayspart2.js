002113ff952b8c43fbd93242d491c5a130d7244d
Mohsin Ghaffar <mohsinghaffar546@gmail.com>
2024-08-12 01:10:33 +0200

    Learned more about arrays methods, push, concate, spreadOperator, flat, Array.isArray, from, of.

---
+++ arrayspart2.js
@@ -0,0 +1,17 @@
+//Push, Concatenate, Spread operator, Array.from,of, array.flat(infinity)
+// let newArr = ["Ahmad","Mohsin","Raza","Nadeem",[2,3,5,6],[4,9,8,45]];
+// let newArr2 = [1,2,4,6];
+// newArr.push("Nabi");
+// let newArr3 = newArr.concat(newArr2);
+// let newArrSpreadOperator = newArr.concat(...newArr2);
+// let flatArray = newArr.flat(Infinity);
+// console.log(flatArray);
+
+// let checkArr = Array.isArray("Mohsin");
+// let checkArr2 = Array.from("Mohsin");
+// let check3 = Array.from({name:"Mohsin"}) //interesting
+let element1 = "Mohsin";
+let element2 = "Ghaffar";
+let element3 = "A Student";
+let addAll = Array.of(element1,element2,element3);
+console.log(addAll);
