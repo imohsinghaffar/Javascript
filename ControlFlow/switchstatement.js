42420ebf1ac527166fa9a06167ada04994316d6c
Mohsin Ghaffar <mohsinghaffar546@gmail.com>
2024-08-17 06:48:14 +0200

    Practiced Switch Stement and understand concept of break, why it is important.

---
+++ ControlFlow/switchstatement.js
@@ -0,0 +1,22 @@
+let month = "August";
+switch (month) {
+    case "Jan":
+        console.log("Your month of birth is January");
+    case "Feb":
+        console.log("Your month of birth is Feb");
+    case "March":
+        console.log("Your month of birth is March");
+    case "April":
+        console.log("Your month of birth is April");
+    case "August":
+        console.log("Your month of birth is August");
+        break;
+    case "Sep":
+        console.log("Your month of birth is September");
+        
+    default:
+        console.log("Don't find your month of birth!");
+        break;
+}
+
+
