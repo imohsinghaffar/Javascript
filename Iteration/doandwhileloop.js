b1a98cfaf4777ecffcd8ca6c018923eb7ae07b10
Mohsin Ghaffar <mohsinghaffar546@gmail.com>
2024-08-18 13:02:38 +0200

    dowhile loop

---
+++ Iteration/doandwhileloop.js
@@ -0,0 +1,18 @@
+let num = 1;
+while (num<=10) {
+    // console.log(`${num} Hello`);
+    num++
+    // console.log(i);
+}
+
+
+//dowhile loop
+let loopValue = 100;
+do {
+    console.log(loopValue);
+    if(loopValue==150)
+    {
+        break;
+    }
+    loopValue++;
+} while (loopValue<=200);
