ac850ab4d2d89c3bc771861e233f35fd5d8f2f33
imohsinghaffar <mohsinghaffar546@gmail.com>
2024-08-26 12:15:30 +0200

    Learned Advance JS concepts of promises with then and catch, and try and catch.

---
+++ Advance JavaScript/one.js
@@ -0,0 +1,85 @@
+let learnPromisesOne = new Promise(function(resolve, reject){
+
+    setTimeout(() => {
+        //console.log("Promise is completed");
+        resolve(); //(1)
+    }, 1000);
+
+})
+//Untill we don't use resolved method in the main promise untill then we cannot print out
+//this promise consumed msg. First main task will complete then this then will output(1).
+learnPromisesOne.then(()=>{
+    //console.log("Promise Consumed!");
+    
+})
+
+
+let learnSecondWay = new Promise((resolve, reject)=>{ //if we have to get data defined in resolve then we have
+    //to use promise without storing it in variable then it will work
+    setTimeout(()=>{
+        //console.log("This Async is second method");
+        resolve({username: "Mohsin Ghaffar", passw: 123}) //If we have to pass any data then we pass in resolve in object form
+    }, 1000)
+})
+
+learnSecondWay.then((userData)=>{ //now we will access above data here
+    //console.log(userData);
+    //return userData
+}) 
+
+//Third Way
+let learnThirdWay = new Promise((resolve, reject)=>{ 
+    setTimeout(()=>{
+        let error = false;
+        if(!error){
+            resolve({username: "Mohsin Ghaffar", passw: 123});        
+        }
+        else{
+            reject("There is an Error!");
+        }
+    }, 1000)
+})
+
+learnThirdWay
+.then((printResult)=>{
+    //console.log(printResult);
+    return printResult.username
+})
+.then((userFullName)=>{
+//console.log(userFullName);
+})
+.catch((err)=>{
+    //console.log(err);
+})
+.finally(()=>{
+    //console.log("All Set No Error!");
+    
+})
+
+
+//Fourth Way
+let newpromiseFour = new Promise((resolve, reject)=>{
+    setTimeout(() => {
+        let checkError = true;
+        if(checkError){
+            resolve({username: "Ali"})
+        }
+        else{
+            reject("There is Something Wrong!")
+        }
+    }, 1000);
+})
+
+async function consumePromise() {
+    try {
+        let checkResult = await newpromiseFour;
+        console.log(checkResult.username);
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
+
+consumePromise() // we have to must call async function otherwise will get error
+
