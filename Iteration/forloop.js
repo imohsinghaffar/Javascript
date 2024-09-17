c229ec368df74671c5ce4c8cc7675910953f60c0
imohsinghaffar <mohsinghaffar546@gmail.com>
2024-09-18 00:06:42 +0200

    practiced forloop deeply

--- Iteration/forloop.js
+++ Iteration/forloop.js
@@ -22,15 +22,52 @@ for(i = 1; i<=10; i++) //outer loop
 }
 
 let fruitsArray = ["Banana", "Mango", "Oranges"];
-console.log(`Array Length is: ${fruitsArray.length}`);
+// console.log(`Array Length is: ${fruitsArray.length}`);
 
 for(i = 0; i<fruitsArray.length; i++)
 {
-    console.log(`${i}: ${fruitsArray[i]}`);
+    // console.log(`${i+1}: ${fruitsArray[i]}`);
     if(fruitsArray[i] == "Mango")
     {
-        console.log(`Array Broked`);
-        break;
-        
+        // console.log(`--> ${i+2}: Array Broked`);
+        // console.log(`--> Array Broked`);
+        continue;
     }
 }
+
+//Write a program that prints the numbers from 1 to 100. 
+//For multiples of 3, print "Fizz" instead of the number, 
+//and for multiples of 5, print "Buzz". For numbers that are 
+//multiples of both 3 and 5, print "FizzBuzz".
+for (let index = 1; index <= 100; index++) {
+    // console.log(index)
+    if(index%3===0 && index%5===0)
+        {
+            // console.log(index, "FuzzBuzz");
+        }
+    else if(index%3===0)
+    {
+        // console.log(index, "Fuzz");
+    }
+    else if(index%5===0)
+    {
+        // console.log(index, "Buzz");
+    }
+    else{
+        // console.log(index, "Not Divisible");
+    }
+}
+
+//Task to find factorial
+function Calcfactorial(num){
+    let result = 1;
+    for(let i = 1;i<=num;i++)
+    {
+       
+        result = result*i
+        // console.log(result);
+        
+    }
+    return result
+}
+console.log(Calcfactorial(3))
